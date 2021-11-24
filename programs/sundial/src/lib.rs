767b1aecd0698cbe733d67606d1d9491fe01282b
Leofis G <gw980531@gmail.com>
2021-11-24 16:41:27 +0800

    Added boiler plate (#2)
    * Added boilerplate code.
    
    * Added init.
    
    * Added field in the sundial account.
    
    * Fixed init sundial.
    
    * Added missing program id.
    
    * Format.

--- programs/sundial/src/lib.rs
+++ programs/sundial/src/lib.rs
@@ -1,14 +1,102 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("SDLxV7m1qmoqkytqYRGY1x438AbYCqekPsPxK4kvwuk");
 
 #[program]
 pub mod sundial {
     use super::*;
-    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        authority_bump: u8,
+        end_unix_time_stamp: u64,
+    ) -> ProgramResult {
+        let sundial = &mut ctx.accounts.sundial.load_init()?;
+        sundial.authority_bump = authority_bump;
+        sundial.end_unix_time_stamp = end_unix_time_stamp;
+        sundial.principle_token_total_supply = 0;
+        sundial.yield_token_total_supply = 0;
+        sundial.total_liquidity_deposited = 0;
+        sundial.principle_token_mint = ctx.accounts.principle_token_mint.key();
+        sundial.yield_token_mint = ctx.accounts.yield_token_mint.key();
+        sundial.liquidity_supply_token_account = ctx.accounts.port_liquidity_supply.key();
+        sundial.collateral_supply_token_account = ctx.accounts.port_collateral_supply.key();
+        sundial.redeem_fee_receiver = ctx.accounts.redeem_fee_receiver.key();
+        sundial.reserve_pubkey = ctx.accounts.reserve_pubkey.key();
+        sundial.token_program = ctx.accounts.token_program.key();
+        Ok(())
+    }
+
+    pub fn mint_principle_tokens_and_yield_tokens(
+        _ctx: Context<MintPrincipleTokenAndYieldToken>,
+    ) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn redeem_principle_tokens(_ctx: Context<RedeemPrincipleToken>) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn redeem_yield_tokens(_ctx: Context<RedeemYieldToken>) -> ProgramResult {
         Ok(())
     }
 }
 
+#[derive(Accounts, Clone)]
+#[instruction(authority_bump: u8, end_unix_time_stamp: u64)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = size_of::< Sundial > () + 8,)]
+    pub sundial: AccountLoader<'info, Sundial>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub owner: AccountInfo<'info>,
+    #[account(seeds=[], bump=authority_bump)]
+    pub sundial_authority: AccountInfo<'info>,
+    #[account(owner=token_program.key())]
+    pub port_liquidity_mint: AccountInfo<'info>,
+    #[account(owner=token_program.key())]
+    pub port_collateral_mint: AccountInfo<'info>,
+    #[account(init, payer=user, token::authority=sundial_authority, token::mint=port_liquidity_mint)]
+    pub port_collateral_supply: AccountInfo<'info>,
+    #[account(init, payer=user, token::authority=sundial_authority, token::mint=port_collateral_mint)]
+    pub port_liquidity_supply: AccountInfo<'info>,
+    #[account(init, payer=user, mint::authority=sundial_authority, mint::decimals=6)]
+    pub principle_token_mint: AccountInfo<'info>,
+    #[account(init, payer=user, mint::authority=sundial_authority, mint::decimals=6)]
+    pub yield_token_mint: AccountInfo<'info>,
+    #[account(init, payer=user, token::authority=sundial_authority, token::mint=port_liquidity_mint)]
+    pub redeem_fee_receiver: AccountInfo<'info>,
+    pub reserve_pubkey: AccountInfo<'info>,
+    #[account(executable)]
+    pub token_program: AccountInfo<'info>,
+    #[account(executable)]
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+    pub clock: Sysvar<'info, Clock>,
+}
+
+#[account(zero_copy)]
+#[derive(Debug, PartialEq)]
+pub struct Sundial {
+    pub authority_bump: u8,
+    pub end_unix_time_stamp: u64,
+    pub principle_token_total_supply: u64,
+    pub yield_token_total_supply: u64,
+    pub total_liquidity_deposited: u64,
+    pub principle_token_mint: Pubkey,
+    pub yield_token_mint: Pubkey,
+    pub liquidity_supply_token_account: Pubkey,
+    pub collateral_supply_token_account: Pubkey,
+    pub redeem_fee_receiver: Pubkey,
+    pub reserve_pubkey: Pubkey,
+    pub token_program: Pubkey,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintPrincipleTokenAndYieldToken {}
+
+#[derive(Accounts)]
+pub struct RedeemPrincipleToken {}
+
+#[derive(Accounts)]
+pub struct RedeemYieldToken {}
--- tests/sundial.ts
+++ tests/sundial.ts
@@ -1,19 +1,118 @@
-import * as anchor from '@project-serum/anchor';
+import { Provider, setProvider, workspace, BN } from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
 import {Sundial, IDL} from '../target/types/sundial';
+import {Keypair, PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, SYSVAR_CLOCK_PUBKEY, Transaction} from '@solana/web3.js';
+import { MintLayout, Token, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 
 describe('sundial', () => {
 
-  anchor.setProvider(anchor.Provider.local());
-  // Configure the client to use the local cluster.
-  const provider = anchor.Provider.local();
+  setProvider(Provider.local());
+  const provider = Provider.local();
 
   const sundial = new Program<Sundial>(
-    IDL, anchor.workspace.Sundial.programId, provider);
+    IDL, workspace.Sundial.programId, provider);
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await sundial.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it ('Initialize Lending Market', async () => {
+    // TODO
+  })
+
+  it ('Initialize Reserve', async () => {
+    // TODO
+  })
+
+  // TODO: prepare the lending market and reserve
+  it('Sundial is initialized!', async () => {
+
+    const sundialAcc = Keypair.generate()
+    const principleTokenMint = Keypair.generate();
+    const yieldTokenMint = Keypair.generate();
+    const liquidityTokenSupply = Keypair.generate();
+    const collateralTokenSupply = Keypair.generate();
+    const redeemFeeReceiver = Keypair.generate();
+    const owner = Keypair.generate();
+    const reservePubkey = Keypair.generate();
+    const [sundialAuthority, nounce] = await PublicKey.findProgramAddress(
+      [],
+      sundial.programId
+    );
+    const portLiquidityMint = await createAccount(
+      provider,
+      MintLayout.span,
+      TOKEN_PROGRAM_ID
+    );
+    const portCollateralMint = await createAccount(
+      provider,
+      MintLayout.span,
+      TOKEN_PROGRAM_ID,
+    );
+
+    await sundial.rpc.initialize(
+      nounce,
+      new BN(1637318787),
+      {
+        accounts: {
+          sundial: sundialAcc.publicKey,
+          sundialAuthority: sundialAuthority,
+          portLiquiditySupply: liquidityTokenSupply.publicKey,
+          portCollateralSupply: collateralTokenSupply.publicKey,
+          principleTokenMint: principleTokenMint.publicKey,
+          yieldTokenMint: yieldTokenMint.publicKey,
+          portLiquidityMint: portLiquidityMint.publicKey,
+          portCollateralMint: portCollateralMint.publicKey,
+          redeemFeeReceiver: redeemFeeReceiver.publicKey,
+          reservePubkey: reservePubkey.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: SystemProgram.programId,
+          rent: SYSVAR_RENT_PUBKEY,
+          user: provider.wallet.publicKey,
+          owner: owner.publicKey,
+          clock: SYSVAR_CLOCK_PUBKEY,
+        },
+        instructions: [
+          Token.createInitMintInstruction(
+            TOKEN_PROGRAM_ID,
+            portLiquidityMint.publicKey,
+            6,
+            provider.wallet.publicKey,
+            provider.wallet.publicKey,
+          ),
+          Token.createInitMintInstruction(
+            TOKEN_PROGRAM_ID,
+            portCollateralMint.publicKey,
+            6,
+            provider.wallet.publicKey,
+            provider.wallet.publicKey,
+          ),
+        ],
+        signers: [
+          sundialAcc, 
+          principleTokenMint, 
+          yieldTokenMint, 
+          liquidityTokenSupply, 
+          collateralTokenSupply, 
+          redeemFeeReceiver
+        ]
+      }
+    );
   });
 });
+
+const createAccount = async (provider: Provider, space: number, owner: PublicKey): Promise<Keypair> => {
+  const newAccount = Keypair.generate();
+  const createTx = new Transaction().add(
+      SystemProgram.createAccount({
+          fromPubkey: provider.wallet.publicKey,
+          newAccountPubkey: newAccount.publicKey,
+          programId: owner,
+          lamports: await provider.connection.getMinimumBalanceForRentExemption(
+              space
+          ),
+          space,
+      })
+  );
+  await provider.send(
+    createTx,
+    [newAccount]
+  );
+  return newAccount
+}
