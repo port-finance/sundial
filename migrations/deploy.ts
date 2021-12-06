b127aaeb2ce4d7991f39f91dcb0301bef38128b7
Zuobin Wang <49583235+Dreamerryao@users.noreply.github.com>
2021-12-06 10:51:42 +0800

    fix(deploy): Fixed migration script(#24)
    * fix(deploy): add console
    
    * fix(deploy): fix problems

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -2,7 +2,7 @@
 // single deploy script that's invoked from the CLI, injecting a provider
 // configured from the workspace's Anchor.toml.
 import { DEFAULT_RESERVE_CONFIG } from "../tests/constants";
-import {createMintAndVault} from "@project-serum/common"
+import { createMintAndVault } from "@project-serum/common";
 import * as anchor from "@project-serum/anchor";
 import { createDefaultReserve, createLendingMarket } from "../tests/utils";
 import { SolanaProvider } from "@saberhq/solana-contrib";
@@ -10,16 +10,28 @@ import { SundialSDK } from "../src";
 import { ReserveParser } from "@port.finance/port-sdk/lib/parsers/ReserveParser";
 import { ReserveData } from "@port.finance/port-sdk/lib/structs/ReserveData";
 import { ParsedAccount } from "@port.finance/port-sdk/lib/parsers/ParsedAccount";
+import { Keypair } from "@solana/web3.js";
 
 module.exports = async function (provider) {
   anchor.setProvider(provider);
   console.log("Provider public key: ", provider.wallet.publicKey.toString());
   const lendingMarket = await createLendingMarket(provider);
+  console.log("marketPublicKey", lendingMarket.publicKey.toString());
   const [mintPubkey, vaultPubkey] = await createMintAndVault(
-    provider, new anchor.BN(1000000000000), provider.wallet.publicKey, 6);
+    provider,
+    new anchor.BN(1000000000000),
+    provider.wallet.publicKey,
+    6
+  );
   const reserveState = await createDefaultReserve(
-    provider, 1, vaultPubkey, lendingMarket.publicKey, DEFAULT_RESERVE_CONFIG);
-
+    provider,
+    1,
+    vaultPubkey,
+    lendingMarket.publicKey,
+    DEFAULT_RESERVE_CONFIG
+  );
+  console.log("mintPubkey", mintPubkey.toString());
+  console.log("reserveState", reserveState.address.toString());
   const solanaProvider = SolanaProvider.load({
     connection: provider.connection,
     sendConnection: provider.connection,
@@ -32,21 +44,17 @@ module.exports = async function (provider) {
 
   const raw = {
     pubkey: reserveState.address,
-    account: await provider.connection.getAccountInfo(reserveState.address)
-  }
+    account: await provider.connection.getAccountInfo(reserveState.address),
+  };
   const reserveInfo = ReserveParser(raw) as ParsedAccount<ReserveData>;
-
-  const createTx = await sundialSDK.sundial.createSundial(
-    {
-      name: "USDC",
-      owner: provider.wallet.publicKey,
-      durationInSeconds: new anchor.BN(1849276800), // 8th of August 2028
-      liquidityMint: mintPubkey,
-      reserve: reserveInfo
-    }
-  );
-
-  await provider.send(
-    createTx.build()
-  );
-}
+  const sundialKeypair = await Keypair.generate();
+  const createTx = await sundialSDK.sundial.createSundial({
+    sundialBase: sundialKeypair,
+    owner: provider.wallet.publicKey,
+    durationInSeconds: new anchor.BN(1849276800), // 8th of August 2028
+    liquidityMint: mintPubkey,
+    reserve: reserveInfo,
+  });
+  console.log("sundialKeypair publicKey: ", sundialKeypair.publicKey.toString());
+  await createTx.confirm();
+};
